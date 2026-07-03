f53f186a49d9aa640e8e71fc56ea73686498c960
“Hemant <harshselakr789@gmial.com>
2026-07-03 15:35:18 +0530

    forgot page done

--- project-3/src/app/forgot/forgot.component.ts
+++ project-3/src/app/forgot/forgot.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-forgot',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './forgot.component.html',
   styleUrl: './forgot.component.css'
 })
