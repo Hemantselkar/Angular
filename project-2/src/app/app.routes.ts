14f6ea52bb5d8fcda8ce39b9742720d8678ff3d1
“Hemant <harshselakr789@gmial.com>
2026-07-08 15:23:43 +0530

    home added

--- project-2/src/app/app.routes.ts
+++ project-2/src/app/app.routes.ts
@@ -3,10 +3,14 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { ProfileComponent } from './profile/profile.component';
 import { EditComponent } from './edit/edit.component';
 import { AddComponent } from './add/add.component';
+import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
     {
-        path:'',redirectTo:'dashboard',pathMatch:'full'
+        path:'',redirectTo:'home',pathMatch:'full'
+    },
+    {
+        path:'home',component:HomeComponent
     },
     {
         path:'dashboard',component:DashboardComponent,children:[
