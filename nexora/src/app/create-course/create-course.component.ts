ea140c3e05eb8d57a413edf84134521499168409
“Hemant <harshselakr789@gmial.com>
2026-07-09 15:08:55 +0530

    done with basic structure

--- nexora/src/app/create-course/create-course.component.ts
+++ nexora/src/app/create-course/create-course.component.ts
@@ -3,11 +3,13 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import {MatCardModule} from '@angular/material/card';
+import {MatRadioModule} from '@angular/material/radio';
 
 @Component({
   selector: 'app-create-course',
   standalone: true,
-  imports: [MatIconModule, MatFormFieldModule, MatInputModule,MatSlideToggleModule ],
+  imports: [MatIconModule, MatFormFieldModule, MatInputModule, MatSlideToggleModule,MatCardModule,MatRadioModule],
   templateUrl: './create-course.component.html',
   styleUrls: ['./create-course.component.scss']
 })
