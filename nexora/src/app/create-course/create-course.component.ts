e819dc9f82a3e7feec981d6e81c907cd2f0150bf
“Hemant <harshselakr789@gmial.com>
2026-07-09 17:33:11 +0530

    redu undo logic

--- nexora/src/app/create-course/create-course.component.ts
+++ nexora/src/app/create-course/create-course.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
@@ -14,5 +14,18 @@ import {MatRadioModule} from '@angular/material/radio';
   styleUrls: ['./create-course.component.scss']
 })
 export class CreateCourseComponent {
+  @ViewChild('editorArea') editorArea!: ElementRef<HTMLTextAreaElement>;
 
+  activeFormats = {
+    bold: false,
+    italic: false,
+    underline: false,
+    strikeThrough: false
+  }
+
+  execCmd(command: string, value?: string): void {
+  
+    document.execCommand(command, false, value);
+ 
+  }
 }
